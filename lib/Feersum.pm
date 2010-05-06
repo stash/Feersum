e3466045f77ec26ecabe48aa7a9fe0ef240732fc
Stash <jeremy.stashewsky@socialtext.com>
2010-05-05 23:53:28 -0700

    Give credit to Kazuho Oku

--- lib/Feersum.pm
+++ lib/Feersum.pm
@@ -200,4 +200,7 @@ This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
 at your option, any later version of Perl 5 you may have available.
 
+picohttpparser is from C<HTTP::Parser::XS> and is Copyright 2009 Kazuho Oku.
+It is released under the same terms as Perl itself.
+
 =cut
